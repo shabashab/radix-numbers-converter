baf242eaf61a7295f973b11b30c219d91e505753
Artem Tarasenko <shabashab.04@gmail.com>
2022-02-03 13:16:14 +0200

    feat: add validation

--- src/edu/ukma/tarasenko/RadixNumberConverter.java
+++ src/edu/ukma/tarasenko/RadixNumberConverter.java
@@ -1,23 +1,18 @@
 package edu.ukma.tarasenko;
 
 import java.math.BigDecimal;
-import java.util.Locale;
+import java.math.RoundingMode;
 
 public class RadixNumberConverter {
-  private final int maxFractionDigitsCount = 32;
   private final char[] chars;
 
   public RadixNumberConverter() {
     chars = "0123456789abcdef".toCharArray();
   }
 
-  private String prepareString(String input) {
-    return input.toLowerCase(Locale.ROOT);
-  }
-
   private int evaluateCharacter(char value) {
-    for(int i = 0; i < value; i++) {
-      if(value == chars[i])  {
+    for (int i = 0; i < chars.length; i++) {
+      if (value == chars[i]) {
         return i;
       }
     }
@@ -32,7 +27,7 @@ public class RadixNumberConverter {
     for (int digitCounter = 0, stringIterator = preparedStringChars.length - 1; stringIterator >= 0; digitCounter++, stringIterator--) {
       int characterValue = evaluateCharacter(preparedStringChars[stringIterator]);
 
-      result += characterValue * ((int)Math.pow(radix, digitCounter));
+      result += characterValue * ((int) Math.pow(radix, digitCounter));
     }
 
     return result;
@@ -41,10 +36,10 @@ public class RadixNumberConverter {
   private BigDecimal convertStringToDecimal(String input, int radix) {
     String[] parts = input.split("[,.]");
 
-    if(parts.length == 1)
+    if (parts.length == 1)
       return new BigDecimal(convertStringToDecimalInt(input, radix));
 
-    if(parts.length != 2)
+    if (parts.length != 2)
       return new BigDecimal(0);
 
     String integerPart = parts[0];
@@ -55,7 +50,7 @@ public class RadixNumberConverter {
     char[] fractionPartCharArray = fractionPart.toCharArray();
     BigDecimal fractionPartValue = new BigDecimal(0);
 
-    for(int i = 0; i < fractionPart.length(); i++) {
+    for (int i = 0; i < fractionPart.length(); i++) {
       int charValue = evaluateCharacter(fractionPartCharArray[i]);
       fractionPartValue = fractionPartValue.add(new BigDecimal(charValue * (Math.pow(radix, -1 * (i + 1)))));
     }
@@ -66,7 +61,7 @@ public class RadixNumberConverter {
   private String convertIntToString(int input, int radix) {
     StringBuilder resultStringBuilder = new StringBuilder();
 
-    while(input > 0) {
+    while (input > 0) {
       int numberValue = input % radix;
       input /= radix;
       resultStringBuilder.append(chars[numberValue]);
@@ -77,21 +72,25 @@ public class RadixNumberConverter {
     return resultStringBuilder.toString();
   }
 
-  private String convertDecimalToString(BigDecimal input, int radix) {
+  private String convertDecimalToString(BigDecimal input, int radix, int fractionDigitsCount) {
     int integerPart = input.intValue();
     BigDecimal fractionPart = input.subtract(new BigDecimal(integerPart));
+    fractionPart = fractionPart.setScale(15, RoundingMode.FLOOR);
 
     String integerPartString = convertIntToString(integerPart, radix);
 
     StringBuilder fractionPartStringBuilder = new StringBuilder();
 
-    for(int i = 0; i < maxFractionDigitsCount; i++) {
+    for (int i = 0; i < fractionDigitsCount; i++) {
       fractionPart = fractionPart.multiply(new BigDecimal(radix));
 
       int value = fractionPart.intValue();
       fractionPart = fractionPart.subtract(new BigDecimal(value));
 
       fractionPartStringBuilder.append(chars[value]);
+
+      if (fractionPart.compareTo(new BigDecimal(0)) == 0)
+        break;
     }
 
     String fractionPartString = fractionPartStringBuilder.toString();
@@ -99,9 +98,9 @@ public class RadixNumberConverter {
     return integerPartString + "," + fractionPartString;
   }
 
-  private String convertToRadixDouble(String input, int inputRadix, int outputRadix) {
+  private String convertToRadixDouble(String input, int inputRadix, int outputRadix, int fractionDigitsCount) {
     BigDecimal value = convertStringToDecimal(input, inputRadix);
-    return convertDecimalToString(value, outputRadix);
+    return convertDecimalToString(value, outputRadix, fractionDigitsCount);
   }
 
   private String convertToRadixInt(String input, int inputRadix, int outputRadix) {
@@ -109,9 +108,34 @@ public class RadixNumberConverter {
     return convertIntToString(value, outputRadix);
   }
 
+  //Returns a string with an error or empty string if no error
+  private String validateString(String input) {
+    if(input.matches("[0-9a-z]+[,.][0-9a-z]+"))
+      return "";
+
+    return "Invalid string format";
+  }
+
+  private boolean checkIsStringNumberHasFractionPart(String input) {
+    return input.matches("[0-9a-z]+[,.][0-9a-z]+");
+  }
+
+  private String prepareString(String input) {
+    return input.toLowerCase();
+  }
+
   public String convertToRadix(String input, int inputRadix, int outputRadix) {
-    if(input.matches("[0-9]+[,.][0-9]+")) {
-      return convertToRadixDouble(input, inputRadix, outputRadix)   ;
+    String preparedString = prepareString(input);
+
+    final int fractionDigitsCount = 250;
+
+    String validationResult = validateString(preparedString);
+
+    if (validationResult.length() != 0)
+      throw new IllegalArgumentException("An error occurred while validating input string: " + validationResult);
+
+    if (checkIsStringNumberHasFractionPart(preparedString)) {
+      return convertToRadixDouble(input, inputRadix, outputRadix, fractionDigitsCount);
     }
 
     return convertToRadixInt(input, inputRadix, outputRadix);
